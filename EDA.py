5a5e142ab7389811506d7fb78ba4579da629784f
Zaki <117178074+zakizndn@users.noreply.github.com>
2023-11-08 14:51:01 +0800

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -1,8 +1,7 @@
 import pandas as pd                         
 import numpy as np                          
 import seaborn as sns                       
-import matplotlib.pyplot as plt             
-%matplotlib inline     
 sns.set(color_codes=True)
 df = pd.read_csv("diabetes.csv")
-df.head(5)  
+print(df.head(5))
+sns.boxplot(x=df['Age'])
